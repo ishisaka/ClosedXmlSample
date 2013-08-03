c31979df0a8728e07e9fe69fd67c267f5cd70570
Tadahiro Ishisaka <ishisaka@gmail.com>
2013-08-03 16:57:03 +0900

    ClosedXMLの使用サンプル ワークブックの作成とセル値の取得

---
+++ ClosedXMLSample/ClosedXmlSample1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ClosedXML.Excel;
+
+namespace ClosedXMLSample
+{
+    /// <summary>
+    /// ClosedXMLを使用してExcelのファイルを作成する。
+    /// </summary>
+    class ClosedXmlSample1
+    {
+        static void Main(string[] args) {
+            //WorkBookオブジェクトを追加
+            using (var wb = new XLWorkbook()) {
+                //WorkSheetをWorkBookオブジェクトに追加
+                var ws = wb.Worksheets.Add("Sample");
+                //セルの値を変更
+                ws.Cell("A1").Value = "Hello World";
+                //作業内容を保存
+                wb.SaveAs("HelloWorld.xlsx");
+            }
+
+        }
+    }
+}
---
+++ ClosedXmlSample2/ClosedXmlSample2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using ClosedXML.Excel;
+
+
+namespace ClosedXmlSample
+{
+    /// <summary>
+    /// ClosedXMLを使用し、Excelからセルの値を取得する
+    /// </summary>
+    class ClosedXmlSample2
+    {
+        static void Main(string[] args) {
+            string filePath = args[0];      //ファイルパス
+            string sheetName = args[1];     //データを読み込むセルがあるワークシートの名前
+            string addressName = args[2];   //ワークシートのセル位置
+
+            using (var wb = new XLWorkbook(filePath)) {
+                var ws = wb.Worksheets.First(s => s.Name == sheetName);
+                string format;
+                //セルの型がわかるので処理がしやすい
+                switch (ws.Cell(addressName).DataType) {
+                    case XLCellValues.Text:
+                        Console.WriteLine("Excel Data Type is Text.");
+                        Console.WriteLine(ws.Cell(addressName).Value);
+                        break;
+                    case XLCellValues.Number:
+                        Console.WriteLine("Excel Data Type is Number.");
+                        format = ws.Cell(addressName).Style.NumberFormat.Format;
+                        Console.WriteLine("Format: " + format);
+                        Console.WriteLine(ws.Cell(addressName).Value);
+                        break;
+                    case XLCellValues.Boolean:
+                        Console.WriteLine("Excel Data Type is Boolean.");
+                        Console.WriteLine(ws.Cell(addressName).Value);
+                        break;
+                    case XLCellValues.DateTime:
+                        Console.WriteLine("Excel Data Type is DateTime.");
+                        format = ws.Cell(addressName).Style.NumberFormat.Format;
+                        Console.WriteLine("Format: " + format);
+                        //セルのバリューはObject型なので、必要に応じてキャストしてやる
+                        Console.WriteLine(((DateTime)ws.Cell(addressName).Value).ToString());
+                        break;
+                    case XLCellValues.TimeSpan:
+                        Console.WriteLine("Excel Data Type is TimeSpan");
+                        format = ws.Cell(addressName).Style.NumberFormat.Format;
+                        Console.WriteLine("Format: " + format);
+                        Console.WriteLine(((TimeSpan)ws.Cell(addressName).Value).ToString());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
